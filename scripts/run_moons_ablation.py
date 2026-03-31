ed1f0c7f95257e807959b4c24ec7ee9378f5b2c0
agshin98 <fataliyevaqshin@gmail.com>
2026-03-31 21:36:53 +0400

    Create run_moons_ablation.py

---
+++ scripts/run_moons_ablation.py
@@ -0,0 +1,5 @@
+from src.config import FIGURES_DIR, get_hyperparams_config
+from src.data_utils import DataRepository
+from src.models import HiddenLayerClassifier
+from src.trainers import HiddenLayerTrainer
+from src.plotting import DecisionBoundaryPlotter
